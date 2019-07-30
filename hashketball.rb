7d86e532fc733445072246e0ad49617d4541a136
Shane Lonergan <sptlonergan@gmail.com>
2019-07-30 21:11:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,15 +226,15 @@ def big_shoe_rebounds
   game_hash.each do | location, team_data |
     team_data[:players].each do | players |
         players.each do | name, data|
-          if !big_shoe 
+          if !big_shoe
             big_shoe = [name, data[:shoe]]
           if data[:shoe] > big_shoe[1]
             big_shoe = [name, data[:shoe]]
-          end   
+          end
           big_shoe
           binding.pry
-        end 
-    end 
+        end
+    end
   end
 
 end
